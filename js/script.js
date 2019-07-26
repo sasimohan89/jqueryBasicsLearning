eed383be8ccf5c6524e96a05b00789456ff72515
sasimohan89 <sm9474699@gmail.com>
2019-07-26 12:35:11 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  // $(".red-box").fadeOut(2000);  
+  // $(".green-box").fadeOut(2000);
+  // //$(".blue-box").fadeIn(2000);
+  // $(".red-box").fadeIn(1000);
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").fadeTo(1000, 0.5);
+  // $(".blue-box").fadeTo(1000, 0.8);
+  // $(".red-box").fadeToggle();
+  // $(".red-box").fadeToggle();
+  // $(".green-box").fadeOut(1000);
+  // $(".green-box").fadeTo(1000, 0.5);
+  $(".green-box").hide();
+  $(".green-box").toggle();
+});
