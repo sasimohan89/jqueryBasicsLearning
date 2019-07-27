6285095f105c247ed59b34e577ad37b92dc76764
sasimohan89 <sm9474699@gmail.com>
2019-07-27 08:29:41 -0400

    changes from 07/26

--- js/script.js
+++ js/script.js
@@ -7,12 +7,40 @@ $(function() {
   // //$(".blue-box").fadeIn(2000);
   // $(".red-box").fadeIn(1000);
   // $(".red-box").fadeTo(1000, 0.2);
-  // $(".green-box").fadeTo(1000, 0.5);
-  // $(".blue-box").fadeTo(1000, 0.8);
+  // $(".green-box").delay(1000).fadeTo(1000, 0.5);
+  // $(".blue-box").delay(2000).fadeTo(1000, 0.3).fadeOut(1000).delay(500).fadeIn(1000);
   // $(".red-box").fadeToggle();
   // $(".red-box").fadeToggle();
   // $(".green-box").fadeOut(1000);
   // $(".green-box").fadeTo(1000, 0.5);
-  $(".green-box").hide();
-  $(".green-box").toggle();
+  // $(".green-box").hide(1000);
+  // $(".green-box").toggle(1000);
+  // $(".green-box").slideUp(2000);
+  // $(".green-box").slideDown(1000);
+  // $("p").hide();
+  // $("p").slideDown(1000);
+  // $("p").slideToggle(1000);
+
+  // $(".blue-box").animate({
+  //   marginLeft : "+=200px",
+  //   "height" : "50px",
+  //   opacity : "0",
+  //   "width" : "50px",
+  //   marginTop : "25px"
+  // }, 1000, "linear");
+  // $("p").animate({
+  //   "font-size" : "+=20px"
+  // }, 1000, "linear");
+  // $("p").animate({
+  //   "font-size" : "-=20px"
+  // }, 1000, "linear");
+
+  // $(".green-box").delay(1000).fadeTo(1000, 0, function(){
+  //   $(".red-box").fadeTo(1000, 0, function(){
+  //     $(".blue-box").fadeTo(1000, 0);
+  //   });
+  // });
+
+  $(".lightbox").delay(500).fadeTo(1000, 2);
+
 });
